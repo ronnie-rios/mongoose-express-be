8faa9aca014256b60347a89bd92c0b095a160cff
Ronnie Rios <ronniesmail14@gmail.com>
2023-01-06 08:29:56 -0600

    get all and post

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,1 +1,21 @@
-console.log('first')
+const express = require('express');
+const router = express.Router();
+const Events = require('../models/Event');
+
+router.get('/', async (req, res) => {
+    const events = await Event.find();
+    return res.json(events);
+});
+
+router.post('/', async (req, res) => {
+    const newEvent = await Event.create(req.body);
+    if (!req.body.name || !req.body.desc) {
+        return res.json({
+            err: 'please enter at the role and company'
+        })
+    } else {
+        return res.json(newEvent)
+    }
+});
+
+module.exports = router;
