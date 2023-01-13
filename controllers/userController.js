19ec01c6045254029068df5147bbae6962dcfc29
Ronnie Rios <ronniesmail14@gmail.com>
2023-01-13 16:01:56 -0600

    pkg instll

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,32 +1,3 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    hashedPassword: {
-        type: String,
-        required: true,
-    },
-    token: String,
-    address: String,
-    Phone: String,
-    favoriteGame: String,
-    why: Text,
-    stores: [],
-    games: [],
-    birthdate: String
-}, { timestamps: true });
-
-module.exports = mongoose.model('User', userSchema);
-
 const express = require('express');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
