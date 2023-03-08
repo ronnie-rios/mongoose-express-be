18a60178b5d30042e404a48c506bfb89f6500d4a
Ronnie Rios <ronniesmail14@gmail.com>
2023-03-07 20:25:32 -0600

    single event route

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -7,6 +7,15 @@ router.get('/', async (req, res) => {
     return res.json(events);
 });
 
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const singleEvent = await Events.findById(id);
+    if(!singleEvent) {
+        res.status(404).json({ err: 'not found'})
+    }
+    return res.json(singleEvent);
+});
+
 router.post('/', async (req, res) => {
     const newEvent = await Events.create(req.body);
     if (!req.body.name || !req.body.location) {
